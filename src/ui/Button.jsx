28f223b0b36b6ea6e084f9565b5bbc18bd61fb0b
Your Name <farhamostafa657@gmail.com>
2025-04-09 17:09:16 +0400

    updating the cart quantity

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,6 +8,7 @@ function Button({ children, disabled, to, type, onClick }) {
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4 ",
     small: base + " py-2 md:py-2.5 md:px-5 text-xs",
+    round: base + " px-2.5  py-1 md:py-2 md:px-3.5 text-xs",
     secondary:
       " px-4 py-3 md:px-6 md:py-4 transition-all text-sm  inline-block rounded-full     font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed focus:text-stone-50 ",
   };
